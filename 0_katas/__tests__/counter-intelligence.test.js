662d25be450a62e408a46acd2d630dd75b64efd8
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-11-03 17:01:57 +0000

    counter intelligence

--- 0_katas/__tests__/counter-intelligence.test.js
+++ 0_katas/__tests__/counter-intelligence.test.js
@@ -1,7 +1,32 @@
 const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
-  it('', () => {
-    // first test here
+  it('should decode x', () => {
+    expect(counterIntelligence('Y')).toBe('X');
+  });
+  it('should decode a single letter', () => {
+    expect(counterIntelligence('BCD Y')).toBe('ABC X');
+  });
+  it('should handle multiple words with a bigger shift value', () => {
+    expect(counterIntelligence('NKRRU YCKKZNKGXZ D')).toBe(
+      'HELLO SWEETHEART X'
+    );
+  });
+  it('should be able to handle punctuation', () => {
+    expect(
+      counterIntelligence(
+        'ANVNVKNA CX YRLT DY IDLLQRWR XW HXDA FJH QXVN OAXV FXAT, MJAURWP G'
+      )
+    ).toBe(
+      'REMEMBER TO PICK UP ZUCCHINI ON YOUR WAY HOME FROM WORK, DARLING X'
+    );
+  });
+  it('should decode regardless of casing', () => {
+    expect(counterIntelligence('ngbk g toik jge :) d')).toBe(
+      'HAVE A NICE DAY :) X'
+    );
+  });
+  it('should decode if it underflows', () => {
+    expect(counterIntelligence('Rovvy Ofobiyxo H')).toBe('HELLO EVERYONE X');
   });
 });
--- 0_katas/counter-intelligence.js
+++ 0_katas/counter-intelligence.js
@@ -1,6 +1,27 @@
 // Please do not change the name of this function
 const counterIntelligence = (str) => {
-  // Your code here
+  str = str.toUpperCase();
+  const shiftVal = 88 - str.charCodeAt(str.length - 1);
+  let decodedStr = '';
+  for (let i = 0; i < str.length; i++) {
+    const currentLetter = str[i];
+    const letterRegEx = /[A-Z]/;
+    if (!currentLetter.match(letterRegEx)) {
+      decodedStr += currentLetter;
+    } else {
+      const currentCode = currentLetter.charCodeAt(0);
+      let newCode = currentCode + shiftVal;
+      if (newCode > 90) {
+        newCode = newCode - 26;
+      } else if (newCode < 65) {
+        // newCode = newCode + 26;
+      }
+      decodedStr += String.fromCharCode(newCode);
+    }
+  }
+  //Upper limit =90
+  //lower limit = 65
+  return decodedStr;
 };
 
 module.exports = { counterIntelligence };
